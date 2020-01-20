57e5908c84d95e4e4f38a462e7888dbc17712794
Davy Biron <sfu1982@gmail.com>
2020-01-20 09:37:45 +0100

    resolve conflicts

--- public/js/arrowup.js
+++ public/js/arrowup.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 const arrowup = document.querySelector('.arrowup');
 
 arrowup.addEventListener('click', () => {
@@ -9,16 +8,4 @@ arrowup.addEventListener('click', () => {
         behavior: "smooth"
     })
 
-=======
-const arrowup = document.querySelector('.arrowup');
-
-arrowup.addEventListener('click', () => {
-
-    window.scrollTo({
-        top: 0,
-        left: 0,
-        behavior: "smooth"
-    })
-
->>>>>>> 0af437dd09ab9dff1e1e6de14c955a03650e0bfc
 })
--- public/js/popup.js
+++ public/js/popup.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 function popup(hideorshow) {
     if (hideorshow == 'hide') {
         document.getElementById('cookie').style.display = "none";
@@ -14,20 +13,3 @@ function pop () {
     }, 0);
 }
 
-=======
-function popup(hideorshow) {
-    if (hideorshow == 'hide') {
-        document.getElementById('cookie').style.display = "none";
-    }
-    else  if(localStorage.getItem("done") == null) {
-        localStorage.setItem("done",1);
-        document.getElementById('cookie').removeAttribute('style');
-    }
-}
-function pop () {
-    setTimeout(function pop () {
-        popup('show');
-    }, 0);
-}
-
->>>>>>> 0af437dd09ab9dff1e1e6de14c955a03650e0bfc
